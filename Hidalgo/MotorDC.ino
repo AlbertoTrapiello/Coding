9237c577f44f58662f672bad9e4b8964081d244e
Alberto <albertotrapiello@gmail.com>
2019-05-11 14:56:31 +0200

    Changes for de Motor

--- Hidalgo/MotorDC.ino
+++ Hidalgo/MotorDC.ino
@@ -1,14 +1,46 @@
-int DIR = 2, PWM = 3;
+
+
 class MotorDC
 {
-  int DIR; //pin asociado al sentido de giro
-  int PWM; //pin asociado a la entrada
-
+  int DIR; // pin used to decide in wich direction does the motor rotate
+  int PWM; // pin used to decide the pwm for the motor
+  int K; //Relación de transformación;
+  float lastSpeed; //saves the last speed
+  float Voltage; //Voltage
 public:
   MotorDC()
   {
-    DIR = 2;
-    PWM = 3;
+    DIR = 2; // by default these is supposed to be the direction pin
+    PWM = 3; // by default these is supposed to be the pwm pin
+    Voltage = 12; // by default it's supposed to be connected to 12 V
+    lastSpeed = 0; // by default it's supposed to start stopped
+  }
+  MotorDC(int dir, int pwm, float V = 12): DIR(dir), PWM(pwm), Voltage(V) {}
+  void SetMotorSpeed(float speed) // the speed is supposed to come in rmp
+  {
+    int val; // value that's going to go between 0-255 in relation to the porcentual speed
+    int lastval; // value that's going to go between 0-255 in relation to the porcentual last speed
+    val = map(speed, 0, 100, 0, 255); // transforms the speed to the analog value
+    lastval = map(lastSpeed, 0, 100, 0, 255); // transforms the last speed to the analog value
+    if(val > lastval)
+    {
+      int abs = val - lastval; // determines the increase
+      for (int i = lastval; i <= val; i++)
+      {
+        analogWrite(PWM, i); //accelerates "slowly"
+        delay(50);
+      }
+    }
+    else
+    {
+      int abs = lastval - val; // determines the increase
+      for (int i = val; i >= lastval; i--)
+      {
+        analogWrite(PWM, i); //accelerates "slowly"
+        delay(50);
+      }
+    }
+
   }
 
 };
@@ -17,7 +49,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(PWM, OUTPUT);
   pinMode(DIR, OUTPUT);
-  
+
   Serial.begin(9600);
 }
 
@@ -25,19 +57,4 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(DIR, HIGH);
 
-  for (int value = 0; value < 100; value++)
-  {
-    analogWrite(PWM, map(value, 0, 100, 0, 255));
-    Serial.println(map(value, 0, 100, 0, 255));
-    delay(50);
-  }
-
-   digitalWrite(DIR, LOW);
-
-  for (int value = 0; value < 100; value++)
-  {
-    analogWrite(PWM, map(value, 0, 100, 0, 255));
-    Serial.println(map(value, 0, 100, 0, 255));
-    delay(50);
-  }
 }
