ca9a382c8e872d9335fcd6e695694e4c18828fa7
Alberto <albertotrapiello@gmail.com>
2019-10-14 00:08:42 +0200

    Looking to change the .begin() for another option from this library

--- Hidalgo/MotorDC.ino
+++ Hidalgo/MotorDC.ino
@@ -1,3 +1,8 @@
+//INCLUDES
+#include <TimedAction.h>
+#include <Vector.h>
+
+//DEFINES
 #define SLOW 150
 #define FAST 50
 #define MAX_ANGLE 340 // maximum angle that the potentiometer can measure correctly
@@ -57,208 +62,25 @@ int CheckSpeed (int Speed, int Voltage) // Simple check so that the speed isn't 
   return -1;
 }
 
-//OBSERVABLE
-
-class Observable
-{
-	bool changed; // variable wich states if the observable has Changed
-	int value; // value to be observed, so that when it changes the Observer can be notified
-	vector <SujetoConcreto> observers; // list of observers
-	int index = 0; // index of the recently created object of the vector
-
-/*
-protected:
-	void setChanged() // NO IDEA
-	{
-
-	}
-	void clearChanged() // NO IDEA
-	{
-
-	}
-*/
-
-public:
-	~Observable()
-	{
-
-	}
-	void setValue(int v)
-	{
-		value = v; // changes the value to the new value
-		Observable::hasChanged(); // notifies the observers
-		cout << "New value: " << value << endl;
-	}
-	int getValue()
-	{
-		return value; // returns the value
-	}
-	void decreaseIndex()
-	{
-		for (int i = 0; i < observers.size(); i++)
-		{
-			if (observers[i].getIndex() > 0)
-			{
-				cout << "The observer[" << i << "] had previously as index: " << observers[i].getIndex() << endl;
-				observers[i].setIndex(observers[i].getIndex() - 1); // it reduces in 1 all the indexes from all the elements from the vector
-				cout << "The observers[" << i << "]'s index is: " << observers[i].getIndex() << endl;
-			}
-		}
-	}
-	void addObserver(SujetoConcreto & o)
-	{
-		o.setIndex(index); // each SujetoConcreto of the vector has it's own position
-		observers.push_back(o); // when a new SujetoConcreto wish to observe the variable it needs to attach to the vector so it can be notified as the rest
-		cout << "The index given to this observer is: " << index << " and the observer has " << o.getIndex() << endl;
-		index++; // each time an element is deleted the index increases
-	}
-	void deleteObserver(SujetoConcreto & o)
-	{
-		if (o.getIndex() < index)
-		{
-			cout << "Deleted observer has " << o.getIndex() << " as index, and the actual index is " << index << endl;
-			observers.erase(observers.begin() + o.getIndex()); // erases the vector's object wich position is the one dictated by the object
-			Observable::decreaseIndex(); // it changes all the indexes from the vector
-			index--; // each time an element is deleted the index decreases
-			cout << "The new index is: " << index << endl;
-		}
-		else
-			cout << "Couldn't delete the observer" << endl; //
-	}
-	void deleteObservers(SujetoConcreto & o)
-	{
-		observers.clear(); // deletes all the objects from the vector
-		index = 0; // if all the elements of the vector are deleted tha inde goes to 0
-	}
-	int countObservers()
-	{
-		return observers.size(); // returns the size of the vector
-	}
-	void hasChanged()
-	{
-		Observable::notifyObservers(value); // when a changes happends it notifies the changes to the observers
-	}
-	void notifyObservers(int & arg)
-	{
-		for (int i = 0; i < observers.size(); i++)
-			observers[i].update(value); // for each element from the vector, it should be responsible of updating himself
-	}
-};
-
-//POTENTIOMETER
-class Potentiometer: public Observable
-{
-  int pin_POT; // pin use to read the potentiometer analog input
-  int input; // variable used to store the analog reads
-  int lastInput; // variable used to store the previous read
-
-public:
-  //CONSTRUCTOR
-  Potentiometer()
-  {
-    pin_POT = A0; // by default these is supposed to be the potentiometer's pin
-  }
-
-  //FUNCTIONS
-  int getpin_POT()
-  {
-    return pin_POT;
-  }
-  float ReadAngle(Direction Dir = NONE)
-  {
-    float angle; // value of the angle read by the analog input
-    if(Potentiometer::CheckNoise(Dir))
-    {
-      angle = map(input, 0, 360, 0, 255); // translates the value of the reading to an angle
-      Observable::hasChanged(); // every time it reads the value of the potentiomenter it notifies the observers
-    }
-    Serial.println("ERROR Angle here can't be negative");
-    return -1; // ERROR
-  }
-  bool CheckNoise(Direction Dir = NONE) // Function responsible to avoid the fake values
-  {
-    bool output[MAX_CHECK];
-    int error = 0;
-
-    for(int i = 0; i < MAX_CHECK; i++)
-    {
-      input = analogRead(A0);
-
-      /*
-      //PRINTED CHECK
-      Serial.println("**********************");
-      Serial.println(i);
-      Serial.print("INPUT: ");
-      Serial.println(input); // prints the value of the analogRead
-      Serial.print("LAST INPUT: ");
-      Serial.println(lastInput); // prints the value of the analogRead
-      Serial.println("**********************");*/
-      if(Dir != NONE)
-      {
-        if(Dir == RIGHT)
-        {
-          if(input >= lastInput)
-            output[i] = true;
-          else
-            output[i] = false;
-        }
-        else if(Dir == LEFT)
-        {
-          if(input <= lastInput)
-            output[i] = true;
-          else
-            output[i] = false;
-        }
-        else
-          output[i] = false;
-        lastInput = input;
-      }
-      //PRINTED CHECK
-      /*
-      Serial.print("Output[i]: ");
-      Serial.println(output[i]);
-      */
-    }
-    for(int i = 0; i< MAX_CHECK; i++)
-    {
-      if(output[i] == false)
-        error++;
-    }
-    /*
-    Serial.print("ERROR = ");
-    Serial.println(error);
-    */
-    if(error > 0 )
-      return false;
-    else
-      return true;
-  }
-  void PrintAngle (Direction Dir = NONE)
-  {
-    Serial.print("The Actual Value of the angle is: ");
-    Serial.println(map(lastInput, 0, MAX_ANGLE, MIN_POT, MAX_POT)); // prints the value of the angle readed
-  }
-};
-
 class Observer // clase interfaz para los observadores
 {
 protected:
-	int Index = -1; // index wich allows to find each observer in the vector
+  int Index = -1; // index wich allows to find each observer in the vector
 
 public:
-	int getIndex()
-	{
-		return Index; // returns the value of the index
-	}
-	void setIndex(int i)
-	{
-		Index = i; // sets the value for the index
-	}
-	~Observer()
-	{
+  int getIndex()
+  {
+    return Index; // returns the value of the index
+  }
+  void setIndex(int i)
+  {
+    Index = i; // sets the value for the index
+  }
+  ~Observer()
+  {
 
-	}
-	virtual void update(int & v) = 0; // every observer can react different to the changes
+  }
+  virtual void update(int & v) = 0; // every observer can react different to the changes
 };
 
 //MOTOR
@@ -378,6 +200,189 @@ public:
     POT_measure = val; // saves the value of the measure in the variable
     Serial.println("Updated the value correctly");
     Serial.println(val);
+  }
+};
+
+//OBSERVABLE
+
+class Observable
+{
+  bool changed; // variable wich states if the observable has Changed
+  int value; // value to be observed, so that when it changes the Observer can be notified
+  Vector <MotorDC> observers; // list of observers
+  int index = 0; // index of the recently created object of the vector
+
+/*
+protected:
+  void setChanged() // NO IDEA
+  {
+
+  }
+  void clearChanged() // NO IDEA
+  {
+
+  }
+*/
+
+public:
+  ~Observable()
+  {
+
+  }
+  void setValue(int v)
+  {
+    value = v; // changes the value to the new value
+    Observable::hasChanged(); // notifies the observers
+    ////cout << "New value: " << value << endl;
+  }
+  int getValue()
+  {
+    return value; // returns the value
+  }
+  void decreaseIndex()
+  {
+    for (int i = 0; i < observers.size(); i++)
+    {
+      if (observers[i].getIndex() > 0)
+      {
+        //cout << "The observer[" << i << "] had previously as index: " << observers[i].getIndex() << endl;
+        observers[i].setIndex(observers[i].getIndex() - 1); // it reduces in 1 all the indexes from all the elements from the vector
+        //cout << "The observers[" << i << "]'s index is: " << observers[i].getIndex() << endl;
+      }
+    }
+  }
+  void addObserver(MotorDC & o)
+  {
+    o.setIndex(index); // each MotorDC of the vector has it's own position
+    observers.push_back(o); // when a new MotorDC wish to observe the variable it needs to attach to the vector so it can be notified as the rest
+    //cout << "The index given to this observer is: " << index << " and the observer has " << o.getIndex() << endl;
+    index++; // each time an element is deleted the index increases
+  }
+  void deleteObserver(MotorDC & o)
+  {
+    if (o.getIndex() < index)
+    {
+      //cout << "Deleted observer has " << o.getIndex() << " as index, and the actual index is " << index << endl;
+      observers.erase(observers.begin() + o.getIndex()); // erases the vector's object wich position is the one dictated by the object
+      Observable::decreaseIndex(); // it changes all the indexes from the vector
+      index--; // each time an element is deleted the index decreases
+      //cout << "The new index is: " << index << endl;
+    }
+    else
+      //cout << "Couldn't delete the observer" << endl; //
+  }
+  void deleteObservers(MotorDC & o)
+  {
+    observers.clear(); // deletes all the objects from the vector
+    index = 0; // if all the elements of the vector are deleted tha inde goes to 0
+  }
+  int countObservers()
+  {
+    return observers.size(); // returns the size of the vector
+  }
+  void hasChanged()
+  {
+    Observable::notifyObservers(value); // when a changes happends it notifies the changes to the observers
+  }
+  void notifyObservers(int & arg)
+  {
+    for (int i = 0; i < observers.size(); i++)
+      observers[i].update(value); // for each element from the vector, it should be responsible of updating himself
+  }
+};
+
+//POTENTIOMETER
+class Potentiometer: public Observable
+{
+  int pin_POT; // pin use to read the potentiometer analog input
+  int input; // variable used to store the analog reads
+  int lastInput; // variable used to store the previous read
+
+public:
+  //CONSTRUCTOR
+  Potentiometer()
+  {
+    pin_POT = A0; // by default these is supposed to be the potentiometer's pin
+  }
+
+  //FUNCTIONS
+  int getpin_POT()
+  {
+    return pin_POT;
+  }
+  float ReadAngle(Direction Dir = NONE)
+  {
+    float angle; // value of the angle read by the analog input
+    if(Potentiometer::CheckNoise(Dir))
+    {
+      angle = map(input, 0, 360, 0, 255); // translates the value of the reading to an angle
+      Observable::hasChanged(); // every time it reads the value of the potentiomenter it notifies the observers
+    }
+    Serial.println("ERROR Angle here can't be negative");
+    return -1; // ERROR
+  }
+  bool CheckNoise(Direction Dir = NONE) // Function responsible to avoid the fake values
+  {
+    bool output[MAX_CHECK];
+    int error = 0;
+
+    for(int i = 0; i < MAX_CHECK; i++)
+    {
+      input = analogRead(A0);
+
+      /*
+      //PRINTED CHECK
+      Serial.println("**********************");
+      Serial.println(i);
+      Serial.print("INPUT: ");
+      Serial.println(input); // prints the value of the analogRead
+      Serial.print("LAST INPUT: ");
+      Serial.println(lastInput); // prints the value of the analogRead
+      Serial.println("**********************");*/
+      if(Dir != NONE)
+      {
+        if(Dir == RIGHT)
+        {
+          if(input >= lastInput)
+            output[i] = true;
+          else
+            output[i] = false;
+        }
+        else if(Dir == LEFT)
+        {
+          if(input <= lastInput)
+            output[i] = true;
+          else
+            output[i] = false;
+        }
+        else
+          output[i] = false;
+        lastInput = input;
+      }
+      //PRINTED CHECK
+      /*
+      Serial.print("Output[i]: ");
+      Serial.println(output[i]);
+      */
+    }
+    for(int i = 0; i< MAX_CHECK; i++)
+    {
+      if(output[i] == false)
+        error++;
+    }
+    /*
+    Serial.print("ERROR = ");
+    Serial.println(error);
+    */
+    if(error > 0 )
+      return false;
+    else
+      return true;
+  }
+  void PrintAngle (Direction Dir = NONE)
+  {
+    Serial.print("The Actual Value of the angle is: ");
+    Serial.println(map(lastInput, 0, MAX_ANGLE, MIN_POT, MAX_POT)); // prints the value of the angle readed
   }
 };
 
