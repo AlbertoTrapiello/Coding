07685fafd05af2efa08f3abc73ab0118828c945d
Alberto <albertotrapiello@gmail.com>
2020-03-16 12:21:48 +0100

    First Commit
    Creating a repository for Soussa's project

--- Hidalgo/MotorDC/MotorDC_easy/PID_control.m
+++ Hidalgo/MotorDC/MotorDC_easy/PID_control.m
@@ -26,7 +26,7 @@ legend({'y_t_a_n','G_Final'});
 %with the data showed in the graph we can calculate the vlues for the
 %empiric models
 
-L = n/m; %the result of solving the x vlaue for y = 0
+L = -n/m; %the result of solving the x vlaue for y = 0
 K = dcgain(G_Final); %The K value is the dc gain
 T = (K-n)/m - L; %% the T is the time passed between the line passsing through y = 0 and the moment when it reaches y = K
 N = 10;
@@ -51,3 +51,52 @@ N = 10;
 [G1_CH,Kp_CH1,Ti_CH1,Td_CH1]=chreswickpid(1,1,[K,L,T,N,0]); %Proportional control
 [G2_CH,Kp_CH2,Ti_CH2,Td_CH2]=chreswickpid(2,1,[K,L,T,N,0]); %Proportional Integral control
 [G3_CH,Kp_CH3,Ti_CH3,Td_CH3]=chreswickpid(3,1,[K,L,T,N,0]); %Proportional Integral Derivative control
+
+%Now that we have the control ssystems we have to arrange the new feeback
+%system to control the motor
+
+H = 1; %the feedback is 11
+
+%ZN
+S1_ZN = feedback(G_Final*G1_ZN,1); 
+S2_ZN = feedback(G_Final*G2_ZN,1);
+S3_ZN = feedback(G_Final*G3_ZN,1);
+
+figure(2); %step(G_Final);
+hold on; step(S1_ZN);
+hold on; step(S2_ZN);
+hold on; step(S3_ZN);
+title('ZN Control');
+xlabel('time (ms)');  ylabel('Angle (degrees)'); %sets the axis labels
+%axis([0 1400 -60 120]);
+legend({'P','PI', 'PID'});
+
+%Cohen Coon
+S1_CC = feedback(G_Final*G1_CC,1); 
+S3_CC = feedback(G_Final*G3_CC,1);
+S4_CC = feedback(G_Final*G4_CC,1);
+
+figure(3); %step(G_Final);
+hold on; step(S1_CC);
+hold on; step(S3_CC);
+hold on; step(S4_CC);
+title('Cohen Coon Control');
+xlabel('time (ms)');  ylabel('Angle (degrees)'); %sets the axis labels
+%axis([0 1400 -60 120]);
+legend({'P','PID', 'PD'});
+
+%Chreswick
+S1_CH = feedback(G_Final*G1_CH,1); 
+S2_CH = feedback(G_Final*G2_CH,1);
+S3_CH = feedback(G_Final*G3_CH,1);
+
+figure(4); %step(G_Final);
+hold on; step(S1_CH);
+hold on; step(S2_CH);
+hold on; step(S3_CH);
+title('Chreswick Control');
+xlabel('time (ms)');  ylabel('Angle (degrees)'); %sets the axis labels
+%axis([0 1400 -60 120]);
+legend({'P','PI', 'PID'});
+
+
