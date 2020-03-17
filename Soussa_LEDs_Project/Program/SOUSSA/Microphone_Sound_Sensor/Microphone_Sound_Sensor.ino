fff0e8c1c7022300611b95d35328af1c6d2c7cff
Alberto <albertotrapiello@gmail.com>
2020-03-17 21:10:19 +0100

    Added a filter with the library

--- Soussa_LEDs_Project/Program/SOUSSA/Microphone_Sound_Sensor/Microphone_Sound_Sensor.ino
+++ Soussa_LEDs_Project/Program/SOUSSA/Microphone_Sound_Sensor/Microphone_Sound_Sensor.ino
@@ -1,16 +1,29 @@
+#include <Filters.h>
 
-void setup() 
+void setup()
 {
   // put your setup code here, to run once:
   pinMode(A0, INPUT);
-  Serial.begin(115200);
+  Serial.begin(19200);
 }
 
-void loop() 
+void loop()
 {
   // put your main code here, to run repeatedly:
+  // filters out changes faster that 5 Hz.
+  float LowfilterFrequency = 20000;
+  float HighfilterFrequency = 20;
+  
+// create a one pole (RC) lowpass filter
+//  FilterOnePole lowpassFilter( LOWPASS, LowfilterFrequency );
+//  FilterOnePole highpassFilter( HIGHPASS, HighfilterFrequency );
+  FilterTwoPole TwoPoleFilter( 20, 1, analogRead(A0));
+
+  // do something else
+
   Serial.print("Reading: ");
-  double a = analogRead(A0);
-  Serial.println(a);
+
+//  Serial.println(highpassFilter.input(lowpassFilter.input( analogRead( A0 ) )));
+  Serial.println(TwoPoleFilter.input(analogRead(A0)));
 
 }
