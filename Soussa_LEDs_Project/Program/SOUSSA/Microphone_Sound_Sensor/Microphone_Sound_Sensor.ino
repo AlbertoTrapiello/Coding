7eb0f381b3aa68072fcd57c2c6b28d51f37078d0
Alberto <albertotrapiello@gmail.com>
2020-03-24 18:37:46 +0100

    Changed the soussa project to an eample using HSV colors to mouch easily control the LED STRIPE

--- Soussa_LEDs_Project/Program/SOUSSA/Microphone_Sound_Sensor/Microphone_Sound_Sensor.ino
+++ Soussa_LEDs_Project/Program/SOUSSA/Microphone_Sound_Sensor/Microphone_Sound_Sensor.ino
@@ -1,9 +1,20 @@
 #include <Filters.h>
+#include <math.h>
+
+#define OFFSET 2.50 
+#define OFFSET_dB 38
+
+int AnalogPin = A0;
+int DigitalPin = 3;
+int Buzzer = 9;
 
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(A0, INPUT);
+  pinMode(AnalogPin, INPUT);
+  pinMode(DigitalPin, INPUT);
+  pinMode(Buzzer, OUTPUT);
+  
   Serial.begin(19200);
 }
 
@@ -13,17 +24,39 @@ void loop()
   // filters out changes faster that 5 Hz.
   float LowfilterFrequency = 20000;
   float HighfilterFrequency = 20;
-  
+
 // create a one pole (RC) lowpass filter
 //  FilterOnePole lowpassFilter( LOWPASS, LowfilterFrequency );
 //  FilterOnePole highpassFilter( HIGHPASS, HighfilterFrequency );
-  FilterTwoPole TwoPoleFilter( 20, 1, analogRead(A0));
-
+  FilterTwoPole TwoPoleFilter( 20, 1, OFFSET);
+  
+  //float FilteredSignal = TwoPoleFilter.input(analogRead(AnalogPin));
+  
+  float VoltageRead = analogRead(AnalogPin) * (5.0/1023.0); 
   // do something else
+  tone(Buzzer, 20000); 
+  
+//  if(digitalRead(DigitalPin))
+//   Serial.println("Extreme value reached");
 
-  Serial.print("Reading: ");
-
-//  Serial.println(highpassFilter.input(lowpassFilter.input( analogRead( A0 ) )));
-  Serial.println(TwoPoleFilter.input(analogRead(A0)));
+/*      
+  float Radius = 0.009;  
+  float Surface = 2 * 3.141592 * Radius * Radius;
+  float Resistor = 24000;  
+  float Power = VoltageRead*VoltageRead/Resistor;
+  float Intensity = Power/Surface;
+  float dB = 10*log10(Intensity/pow(10, -12));
+*/
+  // 2.55 V equals (aproximately) to 38 dB;
+  float IncdB =  10*log((5 - VoltageRead)/OFFSET);
+  float dB = OFFSET_dB + IncdB;
+  Serial.print("Decibels: ");
+//  if((VoltageRead - OFFSET)*100 > 5)
+//  {
+    //Serial.println(dB);
+   Serial.println(VoltageRead);
+//  }
+//  else
+//   Serial.println(0);
 
 }
--- Soussa_LEDs_Project/Program/SOUSSA/SOUSSA.ino
+++ Soussa_LEDs_Project/Program/SOUSSA/SOUSSA.ino
@@ -1,36 +1,68 @@
 #include <FastLED.h>
+#include <Streaming.h>
 
-#define LED_PIN     7
-#define NUM_LEDS    20
+#define LED_PIN     2
+//#define CLOCK_PIN 4
+#define NUM_LEDS    1
+#define LED_TYPE    WS2812
+#define COLOR_ORDER GRB
+#define BRIGHTNESS  255
+
+#define pinHue A0
+#define pinSat A1
+#define pinVal A2
+
 
 CRGB leds[NUM_LEDS];
 
-void setup() 
-{
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  
+
+
+void setup() {
+    Serial.begin(9600);
+    
+    delay(3000);
+    LEDS.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+    //FastLED.setTemperature(Candle);
+    FastLED.setTemperature(0xFF7029);
+    //Candle 0xFF9329 seems OK
+    //Tungsten40W 0xFFC58F way to blue
+    //Tungsten100W 0xFFD6AA way to blue
+    //Halogen 0xFFF1E0 way to blue
+    //CarbonArc 0xFFFAF4 way to blue
+    //HighNoonSun 0xFFFFFB way to blue
+    //DirectSunlight 0xFFFFFF way to blue
+    // OvercastSky 0xC9E2FF way to blue
+    //ClearBlueSky 0x409CFF way to blue
+    // WarmFluorescent 0xFFF4E5 way to blue
+    // StandardFluorescent 0xF4FFFA way to blue
+    // MercuryVapor 0xD8F7FF way to blue
+    //SodiumVapor 0xFFD1B2 way to blue
+    // MetalHalide 0xF2FCFF way to blue
+    // HighPressureSodium 0xFFB74C better but a a bit greenish blue
+    // UncorrectedTemperature 0xFFFFFF  way to blue
+    // Custom 0xFF8029 getting there but still a bit greenish > reduced green(?) in Candle 0xFF9329 : Candle 0x FF 93 29 > Candle 0x FF 80 29
+    // Custom 0xFF6029 nice white with a very slight pink touch
+    // Custom 0xFF5029 getting more pink, no good
+    // Custom 0xFF7029 Yup, looks like a nice white for my leds !
+    FastLED.setBrightness(BRIGHTNESS);
+ 
+    pinMode(pinHue, INPUT);
+    pinMode(pinSat, INPUT);
+    pinMode(pinVal, INPUT);
 }
-void loop() 
-{  
-  leds[0] = CRGB(255, 0, 0);
+
+void loop()
+{
+  byte byteHue = map(analogRead(pinHue), 0, 1023, 0, 255);
+  byte byteSat = map(analogRead(pinSat), 0, 1023, 0, 255);
+  byte byteVal = map(analogRead(pinVal), 0, 1023, 0, 255);
+
+  Serial << byteHue << "  " <<  byteSat << "  " << byteVal << endl;
+  
+  CHSV color = CHSV(byteHue, byteSat, byteVal);
+
+  fill_solid(leds, NUM_LEDS, color);
+  
   FastLED.show();
-  delay(500);  
-  leds[1] = CRGB(0, 255, 0);
-  FastLED.show();
-  delay(500);
-  leds[2] = CRGB(0, 0, 255);
-  FastLED.show();
-  delay(500);
-  leds[5] = CRGB(150, 0, 255);
-  FastLED.show();
-  delay(500);
-  leds[9] = CRGB(255, 200, 20);
-  FastLED.show();
-  delay(500);
-  leds[14] = CRGB(85, 60, 180);
-  FastLED.show();
-  delay(500);
-  leds[19] = CRGB(50, 255, 20);
-  FastLED.show();
-  delay(500);
+  
 }
